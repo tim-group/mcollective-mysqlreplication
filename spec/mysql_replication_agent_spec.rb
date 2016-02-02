d1a8aa26b0423fd11aeef1112db7f68d2579fcf5
Richard Pearce <richard.pearce@timgroup.com>
2016-02-02 11:21:07 +0000

    rpearce: use stringio instead of mocking, make function generic

--- spec/mysql_replication_agent_spec.rb
+++ spec/mysql_replication_agent_spec.rb
@@ -1,6 +1,7 @@
 require 'mcollective'
 require 'rspec'
 require 'open4'
+require 'stringio'
 
 
 describe MCollective::Agent::Mysqlreplication, :mcollective => true do
@@ -10,15 +11,15 @@ describe MCollective::Agent::Mysqlreplication, :mcollective => true do
     @agent = MCollective::Test::LocalAgentTest.new('mysqlreplication', :agent_file => agent_file).plugin
   end
 
-  def expect_mysql_to_run_mock_output(options)
-    stdout = mock
-    stdout.expects(:read).returns(options[:stdout])
+  def expect_to_run_mock_output(options)
+    stdout = StringIO.new(options[:stdout])
+    stderr = StringIO.new(options[:stderr])
+    stdin = StringIO.new
 
     status = mock
     status.expects(:exitstatus).returns(options[:exit_code])
 
-    stderr = options[:stderr]
-    Open4.expects(:popen4).with('mysql', '-e', options[:expected_command]).returns([100, '', stdout, stderr])
+    Open4.expects(:popen4).with(*options[:expected_command]).returns([100, stdin, stdout, stderr])
     Process.expects(:waitpid2).with(100).returns([nil, status])
   end
 
@@ -28,11 +29,11 @@ describe MCollective::Agent::Mysqlreplication, :mcollective => true do
     dummy_output = "*************************** 1. row ***************************\n               Slave_IO_State: Waiting for master to send event\n                  Master_Host: production-timdb-002.pg.net.local\n                  Master_User: replicant\n                  Master_Port: 3306\n                Connect_Retry: 60\n              Master_Log_File: mysqld-bin.007907\n          Read_Master_Log_Pos: 51948862\n               Relay_Log_File: mysqld-relay-bin.022571\n                Relay_Log_Pos: 51949026\n        Relay_Master_Log_File: mysqld-bin.007907\n             Slave_IO_Running: Yes\n            Slave_SQL_Running: Yes\n              Replicate_Do_DB: tradeideasmonitor,percona,tradeideasmonitor\n          Replicate_Ignore_DB: \n           Replicate_Do_Table: \n       Replicate_Ignore_Table: tradeideasmonitor.CACHE\n      Replicate_Wild_Do_Table: \n  Replicate_Wild_Ignore_Table: \n                   Last_Errno: 0\n                   Last_Error: \n                 Skip_Counter: 0\n          Exec_Master_Log_Pos: 51948862\n              Relay_Log_Space: 51949470\n              Until_Condition: None\n               Until_Log_File: \n                Until_Log_Pos: 0\n           Master_SSL_Allowed: No\n           Master_SSL_CA_File: \n           Master_SSL_CA_Path: \n              Master_SSL_Cert: \n            Master_SSL_Cipher: \n               Master_SSL_Key: \n        Seconds_Behind_Master: 0\nMaster_SSL_Verify_Server_Cert: No\n                Last_IO_Errno: 0\n                Last_IO_Error: \n               Last_SQL_Errno: 0\n               Last_SQL_Error: \n  Replicate_Ignore_Server_Ids: \n             Master_Server_Id: 1002\n                  Master_UUID: 27a8e780-91ed-11e5-8516-525400ffc8a7\n             Master_Info_File: /var/lib/mysql/master.info\n                    SQL_Delay: 0\n          SQL_Remaining_Delay: NULL\n      Slave_SQL_Running_State: Slave has read all relay log; waiting for the slave I/O thread to update it\n           Master_Retry_Count: 86400\n                  Master_Bind: \n      Last_IO_Error_Timestamp: \n     Last_SQL_Error_Timestamp: \n               Master_SSL_Crl: \n           Master_SSL_Crlpath: \n           Retrieved_Gtid_Set: \n            Executed_Gtid_Set: \n                Auto_Position: 0"
 
 
-    expect_mysql_to_run_mock_output (
+    expect_to_run_mock_output (
       {
-        :expected_command => 'show slave status \\G',
+        :expected_command => ['mysql', '-e', 'show slave status \\G'],
         :stdout => dummy_output,
-        :sterr => '',
+        :stderr => '',
         :exit_code => 0
       }
     )
