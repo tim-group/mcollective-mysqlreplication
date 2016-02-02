fa35040263b901534da7bfb04b7185f5b0dbfb36
Richard Pearce <richard.pearce@timgroup.com>
2016-02-02 17:08:09 +0000

    rpearce: More rubocop fixes

--- agent/mysqlreplication.rb
+++ agent/mysqlreplication.rb
@@ -13,23 +13,22 @@ module MCollective
       end
 
       def query_mysql(query)
-        pid, _stdin, stdout, stderr = Open4.popen4("mysql", "-e", query)
-        _ignored, status = Process::waitpid2(pid)
+        pid, _stdin, stdout, stderr = Open4.popen4('mysql', '-e', query)
+        _ignored, status = Process.waitpid2(pid)
 
-       reply.statuscode = status.exitstatus
-       reply.data[:contents] = convert_mysql_output_to_hash(stdout.read.strip)
-       reply.fail!(stderr.read.strip, 1) unless status.exitstatus == 0
+        reply.statuscode = status.exitstatus
+        reply.data[:contents] = convert_mysql_output_to_hash(stdout.read.strip)
+        reply.fail!(stderr.read.strip, 1) unless status.exitstatus == 0
       end
 
       def convert_mysql_output_to_hash(lines)
         results = {}
         output = lines.split("\n")
         output.each do |line|
-          if line.include?(':')
-            key, value = line.split(':')
-            value.strip! unless value.nil?
-            results[key.strip.downcase.to_sym] = value
-          end
+          next unless line.include?(':')
+          key, value = line.split(':')
+          value.strip! unless value.nil?
+          results[key.strip.downcase.to_sym] = value
         end
         results
       end
