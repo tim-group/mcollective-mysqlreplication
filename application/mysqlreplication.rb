6138d971a6baa72df370b79c5c0bb39c176abd7e
Richard Pearce <richard.pearce@timgroup.com>
2016-02-03 16:00:37 +0000

    rpearce: Rubocop fixed, remove spurious fail

--- application/mysqlreplication.rb
+++ application/mysqlreplication.rb
@@ -10,51 +10,54 @@ module Validation
   end
 end
 
-class MCollective::Application::Mysqlreplication < MCollective::Application
-  include Validation
+class MCollective
+  class Application
+    class Mysqlreplication < MCollective::Application
+      include Validation
 
-  description 'Retrieves config.properties from application servers'
-  usage <<-USAGE
-  mco mysqlreplication [show_slave_status][show_master_status] [FILTERS]
+      description 'Retrieves config.properties from application servers'
+      usage <<-USAGE
+      mco mysqlreplication [show_slave_status][show_master_status] [FILTERS]
 
-  show_show_status   - read the show slave status information from mysql.
-  show_master_status - read the show master status information from mysql.
+      show_show_status   - read the show slave status information from mysql.
+      show_master_status - read the show master status information from mysql.
 
-  USAGE
+      USAGE
 
-  def post_option_parser(configuration)
-    validated_command = Validation.parse(ARGV)
-    command_as_hash = Hash[validated_command.members.map(&:to_sym).zip(validated_command.to_a)]
-    configuration.merge!(command_as_hash)
-  end
+      def post_option_parser(configuration)
+        validated_command = Validation.parse(ARGV)
+        command_as_hash = Hash[validated_command.members.map(&:to_sym).zip(validated_command.to_a)]
+        configuration.merge!(command_as_hash)
+      end
 
-  def print_config(host, configStr, mtimeStr)
-    printf("%40s: mtime: %s\n", host, mtimeStr)
-    configStr.split("\n").each.with_index do |line, lineNum|
-      printf("%40s:%3s:   %s\n", host, lineNum, line)
-    end
-  end
+      def print_config(host, configStr, mtimeStr)
+        printf("%40s: mtime: %s\n", host, mtimeStr)
+        configStr.split("\n").each.with_index do |line, lineNum|
+          printf("%40s:%3s:   %s\n", host, lineNum, line)
+        end
+      end
 
-  def print_error_response(host, reply)
-    printf('%40s: %s', host, reply)
-  end
+      def print_error_response(host, reply)
+        printf('%40s: %s', host, reply)
+      end
 
-  def log_unsuccessful_requests(response)
-    fail 'log un'
-    print_error_response(response[:sender], response[:statusmsg]) unless response[:statuscode] == 0
-  end
+      def log_unsuccessful_requests(response)
+        print_error_response(response[:sender], response[:statusmsg]) unless response[:statuscode] == 0
+      end
 
-  def main
-    mc = rpcclient('mysqlreplication')
-    mc.fact_filter 'mysql_exists', true
-    mc.send(configuration[:command]).each do |response|
-      if response[:statuscode] == 0
-        print_config(response[:sender], response[:data][:contents])
-      else
-        print_error_response(response[:sender], response[:statusmsg])
+      def main
+        mc = rpcclient('mysqlreplication')
+        mc.fact_filter 'mysql_exists', true
+        mc.send(configuration[:command]).each do |response|
+          if response[:statuscode] == 0
+            print_config(response[:sender], response[:data][:contents])
+          else
+            print_error_response(response[:sender], response[:statusmsg])
+          end
+        end
+        mc.disconnect
+        printrpcstats
       end
     end
-    mc.disconnect
-    printrpcstats
   end
 end
